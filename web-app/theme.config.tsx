b3dd2e82bf1357621444c753ed2dd0395c8bfbe4
salimbeni1 <etienne.salimbeni@epfl.ch>
2024-08-29 16:05:10 +0200

    add template

---
+++ web-app/theme.config.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Ailegis API documentation</span>,
+  docsRepositoryBase: 'https://github.com/ailegis/ailegis-api-doc',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
