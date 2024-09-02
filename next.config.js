d9215783b66512af0e51f82efd02d083289ae50e
salimbeni1 <etienne.salimbeni@epfl.ch>
2024-09-02 22:32:47 +0200

    new next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
-module.exports = withNextra()
+const nextConfig = {
+  swcMinify: true,
+  trailingSlash: true,
+  output: "export",
+};
+
+module.exports = {
+  ...withNextra(),
+  ...nextConfig,
+};
