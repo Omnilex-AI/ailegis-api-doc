0a5e1149e11b08ff0bff8ac7426bf1b2f2fd5d85
salimbeni1 <etienne.salimbeni@epfl.ch>
2024-09-07 21:48:39 +0200

    update doc

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,9 +3,10 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Ailegis API documentation</span>,
-  docsRepositoryBase: 'https://github.com/ailegis/ailegis-api-doc',
+  logoLink: "https://ailegis.ch",
+  docsRepositoryBase: 'https://github.com/ailegis/ailegis-api-doc/blob/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Ailegis API documentation<',
   },
 }
 
