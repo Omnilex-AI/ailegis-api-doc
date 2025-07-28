36ec7a6a774de7b96679a721329be4319d535753
salimbeni1 <etienne.salimbeni@epfl.ch>
2025-07-28 22:39:48 +0200

    update legal research stream events

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,15 +3,15 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Ailegis API documentation</span>,
-  logoLink: "https://ailegis.ch",
-  docsRepositoryBase: 'https://github.com/ailegis/ailegis-api-doc/blob/main',
+  logoLink: "https://omnilex.ai",
+  docsRepositoryBase: 'https://github.com/omnilex-ai/ailegis-api-doc/blob/main',
   footer: {
-    text: 'Ailegis API documentation<',
+    text: 'Omnilex API documentation<',
   },
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="Ailegis API documentation" />
+      <meta property="og:title" content="Omnilex API documentation" />
       <meta property="og:description" content="Legal research API" />
     </>
   )
